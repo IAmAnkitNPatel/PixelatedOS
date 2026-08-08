83bf6b4e9173426490488f4dcdb2d58e7b3d8e6f
IAmAnkitNPatel <ankit.n.patel.work@gmail.com>
2026-08-08 21:37:57 +0530

    wip: working on taskbar grouping for multiple app instances and hover previews.

--- static/scripts/desktop.js
+++ static/scripts/desktop.js
@@ -72,7 +72,7 @@ function homeIconDoubleClick(e) {
   //   minimize(newHomeContainerDiv);
   // });
 
-  
+
   // homeExplorer Body Div
   // homeExplorerBody(newHomeContainerDiv);
   homeExplorer(newHomeContainerDiv);
