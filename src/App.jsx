11dad78e950a0634597606978de2bfc9f5f6bc06
IAmAnkitNPatel <ankit.n.patel.work@gmail.com>
2026-08-20 14:38:16 +0530

    feat: setup Vite React environment and desktop workspace styles

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="desktop">
+      <div className="workspace">
+        {/* Desktop icons and open windows go here */}
+      </div>
+    </div>
+  );
+}
