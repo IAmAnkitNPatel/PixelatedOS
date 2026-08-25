095496ee9bfbb7ba4594ac8d3ca0d56311a27f91
IAmAnkitNPatel <ankit.n.patel.work@gmail.com>
2026-08-25 20:54:28 +0530

    wip: homeAppShell dragging logic

--- src/components/HomeAppShell.jsx
+++ src/components/HomeAppShell.jsx
@@ -1,9 +1,55 @@
-export default function HomeAppShell (props) {
+import { useState, useEffect } from 'react';
 
+export default function HomeAppShell (props) {
+  
   const startX = props.initialPosition.x;
   const startY = props.initialPosition.y;
 
-  console.log("homeappshell corrdinates", startX, startY);
+  // console.log("homeappshell corrdinates", startX, startY);
+
+  const [position, setPosition] = useState(props.initialPosition);
+
+  const [isDragging, setIsDragging] = useState(false);
+  
+  const [dragOffset, setDragOffset] = useState({
+    x: 0,
+    y: 0
+  });
+
+  const handleMouseDown = (e)=>{
+    setIsDragging(true);
+
+    console.log("setIsDragging = ", isDragging);
+
+    // offset is basically distance between the cursor and topleft position of HomeAppShell
+    // Offset is the pixel distance between the mouse cursor and the top-left corner of the window at the moment of click
+
+    console.log("coordinates")
+    console.log(e.clientX);
+    console.log(e.clientY);
+    const offsetX = e.clientX - position.x;
+    const offsetY = e.clientY - position.y;
+
+    setDragOffset({
+      x: offsetX,
+      y: offsetY
+    });
+  };
+
+  useEffect
+
+  const handleMouseMove = (e)=>{
+    if(!setIsDragging) return;
+
+    setPosition({
+      x: e.clientX - dragOffset.x,
+      y: e.clientY - dragOffset.y
+    });
+  }
+  
+  const handleMouseUp = ()=>{
+    setIsDragging(false);
+  }
   return (
     <div
       className="home-app-shell"
@@ -13,7 +59,10 @@ export default function HomeAppShell (props) {
         top: `${startY}px`
       }}
     >
-      <div className="shell-header">
+      <div
+        className="shell-header" 
+        onMouseDown={handleMouseDown}
+      >
         <div className="shell-header-title">{props.appName}</div>
         <div className="shell-header-buttons">
           <div className="shell-minimize-button">▼</div>
