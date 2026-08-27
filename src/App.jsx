ac476803f05047451162d9588edb4cdaa97d7e47
IAmAnkitNPatel <ankit.n.patel.work@gmail.com>
2026-08-27 22:50:51 +0530

    wip: working on taskbar

--- src/App.jsx
+++ src/App.jsx
@@ -37,32 +37,40 @@ export default function App() {
 
 
   return (
-    <div className="desktop">
-      <div className="workspace">
-        <DesktopIcon
-          name="Home Explorer"
-          icon="🏠"
-          openApp={handleOpenHomeExplorer}
-          // openApp={() => setIsOpen(true)}
-        />
-
-        {/* {isOpen && (
-          <HomeAppShell
-            appName="Home Explorer"
-            closeApp={() => setIsOpen(false)}
+    <>
+      <div className="desktop">
+        <div className="workspace">
+          <DesktopIcon
+            name="Home Explorer"
+            icon="🏠"
+            openApp={handleOpenHomeExplorer}
+            // openApp={() => setIsOpen(true)}
           />
-        )} */}
 
-        {openApps.map((app) => (
-          <HomeAppShell
-            key={app.id}
-            appName={app.appName}
-            closeApp={() => handleCloseHomeExplorer(app.id)}
-            initialPosition={app.initialPosition}
-          />
-        ))}
-        
+          {/* {isOpen && (
+            <HomeAppShell
+              appName="Home Explorer"
+              closeApp={() => setIsOpen(false)}
+            />
+          )} */}
+
+          {openApps.map((app) => (
+            <HomeAppShell
+              key={app.id}
+              appName={app.appName}
+              closeApp={() => handleCloseHomeExplorer(app.id)}
+              initialPosition={app.initialPosition}
+            />
+          ))}
+          
+        </div>
       </div>
-    </div>
+      <div className='taskbar'>
+        <div className="taskbar-left-section"></div>
+        <div className="taskbar-middle-section"></div>
+        <div className="taskbar-right-section"></div>
+
+      </div>
+    </>
   );
 }
