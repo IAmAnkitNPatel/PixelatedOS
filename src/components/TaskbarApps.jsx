570f42e7d04fba5d5370b297f783c2f4ebeda45b
IAmAnkitNPatel <ankit.n.patel.work@gmail.com>
2026-08-31 14:35:57 +0530

    style(taskbar): add styles for taskbar app icons

--- src/components/TaskbarApps.jsx
+++ src/components/TaskbarApps.jsx
@@ -1,20 +1,20 @@
 import { Apps } from "../config/apps";
 
 export default function TaskbarApps({app}) {
-  console.log("opened App", app);
-  console.log("", app.icon)
-  console.log(Apps);
+  // console.log("opened App", app);
+  // console.log("", app.icon)
+  // console.log(Apps);
   // const found = Apps.find(a => a.id === app.id); this is for array
   // const found
   // console.log("found : ", found);
 
-  const found = Apps[app.appId];
-  console.log("found" ,found);
-  console.log(found.icon);
+  const appData = Apps[app.appId];
+  // console.log("found" ,found);
+  // console.log(found.icon);
   return(
     <div className="taskbar-app-container">
       <div className="taskbar-app-icon">
-        {found.icon}
+        {appData.icon}
       </div>
       <div className="taskbar-app-instance"></div>
     </div>
